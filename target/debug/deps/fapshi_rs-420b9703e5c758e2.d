28eb618f4193ebb693c52f86c2ad9f1d73f12dc8
Christian_Yemele <yemelechristian2@gmail.com>
2025-05-08 14:40:07 +0100

    feat(): removed unwanted

--- target/debug/deps/fapshi_rs-420b9703e5c758e2.d
+++
@@ -1,5 +0,0 @@
-/home/christian/Desktop/fapshi-rs/target/debug/deps/libfapshi_rs-420b9703e5c758e2.rmeta: src/lib.rs
-
-/home/christian/Desktop/fapshi-rs/target/debug/deps/fapshi_rs-420b9703e5c758e2.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/examples/make_payment-0d9f109caf7ba306.d
+++
@@ -1,5 +0,0 @@
-/home/christian/Desktop/fapshi-rs/target/debug/examples/libmake_payment-0d9f109caf7ba306.rmeta: examples/make_payment.rs
-
-/home/christian/Desktop/fapshi-rs/target/debug/examples/make_payment-0d9f109caf7ba306.d: examples/make_payment.rs
-
-examples/make_payment.rs:
